79f5553fcb55e9d53425cfb45988e88109148ccc
Maksim <Maksim@DESKTOP-DTUL35U>
2022-09-13 21:41:29 +0300

    Добавьте файлы проекта.

---
+++ dz1/Source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+void main(void) {
+	setlocale(LC_ALL, "RUS");
+	
+	puts("            |\\");
+	puts("            | \\");
+	puts("            |  \\");
+	puts("            |   \\");
+	puts("            |    |");
+	puts("            |   /");
+	puts("       0    |  /");
+	puts("      /|\\   | /");
+	puts(" _____/\\____|__________");
+	puts(" \\                    /");
+	puts("  \\                  /");
+	puts("   \\________________/");
+}
